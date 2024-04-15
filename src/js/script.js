259ea10a6ed8c64ceb50f4cad7313a2d9860b6d8
moe <moe.aoki0124@gmail.com>
2024-04-16 03:06:42 +0900

    0416

--- src/js/script.js
+++ src/js/script.js
@@ -15,39 +15,47 @@ jQuery(function ($) {
     $(".js-drawer").fadeOut();
   });
 
-  //スライダー
-  const mv_swiper = new Swiper(".js-mv-swiper", {
-    loop: true,
-    effect: "fade",
-    speed: 3000,
-    allowTouchMove: false,
-    autoplay: {
-      delay: 3000,
-    },
+  // resizeイベント
+  $(window).on("resize", function () {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+      $(".js-hamburger").removeClass("is-open");
+      $(".js-drawer").fadeOut();
+    }
   });
+
+  //スライダー
+  // const mv_swiper = new Swiper(".js-mv-swiper", {
+  //   loop: true,
+  //   effect: "fade",
+  //   speed: 3000,
+  //   allowTouchMove: false,
+  //   autoplay: {
+  //     delay: 3000,
+  //   },
+  // });
 
   //カードスライド
-  var campaign_swiper = new Swiper(".js-campaign-swiper", {
-    loop: true,
-    speed: 2000,
-    slidesPerView: 1.5,
-    spaceBetween: 24,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 3.5,
-        spaceBetween: 40,
-      },
-    },
+  // var campaign_swiper = new Swiper(".js-campaign-swiper", {
+  //   loop: true,
+  //   speed: 2000,
+  //   slidesPerView: 1.5,
+  //   spaceBetween: 24,
+  //   autoplay: {
+  //     delay: 2000,
+  //     disableOnInteraction: false,
+  //   },
+  //   breakpoints: {
+  //     768: {
+  //       slidesPerView: 3.5,
+  //       spaceBetween: 40,
+  //     },
+  //   },
 
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  // });
 
   //ページトップ
   const pageTop = $(".page-top-button");
@@ -69,7 +77,7 @@ jQuery(function ($) {
     return false;
   });
 
-  // フッター手前でストップ
+  フッター手前でストップ;
   $(".page-top-button").hide();
   $(window).on("scroll", function () {
     const scrollHeight = $(document).height();
@@ -87,49 +95,6 @@ jQuery(function ($) {
       });
     }
   });
-
-  // //トップへ戻るボタン
-  // $(function () {
-  //   const pagetop = $(".page-top-button");
-  //   pagetop.hide(); //最初はボタンを非表示
-  //   $(window).scroll(function () {
-  //     if ($(this).scrollTop() > 100) {
-  //       //100px以上スクロールしたら
-  //       pagetop.fadeIn(); //ボタンをフェードイン
-  //     } else {
-  //       pagetop.fadeOut(); //ボタンをフェードアウト
-  //     }
-  //   });
-  //   pagetop.click(function () {
-  //     $("body,html").animate(
-  //       {
-  //         scrollTop: 0, //上から0pxの位置に戻る
-  //       },
-  //       800
-  //     ); //800ミリ秒かけて上に戻る
-  //     return false;
-  //   });
-  // });
-
-  // //フッター手前で止まるボタン
-  // $(document).ready(function () {
-  //   $(".page-top-button").hide();
-  //   $(window).on("scroll", function () {
-  //     scrollHeight = $(document).height(); //ドキュメントの高さ
-  //     scrollPosition = $(window).height() + $(window).scrollTop(); //現在の位置
-  //     footHeight = $("footer").innerHeight(); //フッターの高さ
-  //     if (scrollHeight - scrollPosition <= footHeight) {
-  //       //ドキュメントの高さと現在の位置の差がフッターの高さ以下のとき
-  //       $(".page-top-button").css({
-  //         position: "absolute",
-  //         bottom: footHeight + 0,
-  //       }); //pisitionをabsoluteに変更
-  //     } else {
-  //       //それ以外の場合は
-  //       $(".page-top-button").css({ position: "fixed", bottom: "0" }); //固定で表示
-  //     }
-  //   });
-  // });
 
   // var speed = 700; // speed変数を定義
   // $(".information__img, .voice-card__img, .price__img").each(function () {
